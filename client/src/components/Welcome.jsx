279f0a67c609ff2f5c0be291c891c5058b9004cc
Edilson Morais <moraisbritoedilson@gmail.com>
2022-11-22 16:37:01 -0300

    implementing the navbar

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,6 +1,21 @@
+import {AiFillPlayCircle} from 'react-icons/ai';
+import {SiEthereum} from 'react-icons/si';
+import {BsInfoCircle} from 'react-icons/bs';
+
+// import {Loader} from './';
+
 const Welcome = () => {
     return (
-        <h1>Welcome</h1>
+        <div className='flex w-full justify-center items-center'>
+            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+                <div className="flex flex-1 justify-start flex-col md:mr-10">
+                    <h1 className='text-3xl sm:text-5xl text text-white text-gradient py-1'>
+                        Send crypto <br /> across the globe.
+                    </h1>
+                </div>
+            </div>
+
+        </div>
     )
 }
 
